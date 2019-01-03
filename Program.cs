49d8d8c0f5011fc3ed7ea2927ed181caff09cc65
ShvetsSerhii <44268566+ShvetsSerhii@users.noreply.github.com>
2019-01-03 09:06:27 +0100

    Add files via upload
    A void program added.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_3_1_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
